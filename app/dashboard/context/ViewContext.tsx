f2d8660180069c0cb460b3ef2479893219caa960
Wiwek09 <wastedman01@gmail.com>
2024-10-30 17:18:56 +0545

    feat: added sessionStorage of view to ViewContext

--- app/dashboard/context/ViewContext.tsx
+++ app/dashboard/context/ViewContext.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { createContext, ReactNode, useState } from "react";
+import React, { createContext, ReactNode, useState, useEffect } from "react";
 
 type ViewType = "grid" | "list";
 
@@ -14,7 +14,16 @@ export const ViewContext = createContext<ViewContextType | null>(null);
 
 // Create a provider component to wrap around the layout
 export const ViewProvider = ({ children }: { children: ReactNode }) => {
-  const [view, setView] = useState<ViewType>("list");
+  
+  const [view, setView] = useState<ViewType>(() => {
+    const storedView = sessionStorage.getItem("view") as ViewType;
+    return storedView || "grid";
+  });
+
+  // Update sessionStorage whenever the view state changes
+  useEffect(() => {
+    sessionStorage.setItem("view", view);
+  }, [view]);
 
   return (
     <ViewContext.Provider value={{ view, setView }}>
