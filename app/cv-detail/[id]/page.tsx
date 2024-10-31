31a934f8c8a2d868e57986d0f9c33ad8bc0d50d2
Wiwek09 <wastedman01@gmail.com>
2024-10-31 16:07:52 +0545

    style: increased width of pdf and also adjusted length of pdf view

--- app/cv-detail/[id]/page.tsx
+++ app/cv-detail/[id]/page.tsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 import { GoDotFill } from "react-icons/go";
 import axios from "@/utils/axiosConfig";
 
-
 const CVDetailPage = ({ params }: { params: string }) => {
   const [data, setData] = useState<any>();
   const { id }: any = params;
@@ -26,25 +25,24 @@ const CVDetailPage = ({ params }: { params: string }) => {
   };
 
   return (
-    <div className="h-screen flex space-x-4 w-full">
-      <Card className="w-[50%] bg-gray-100">
-        <div>
+    <div className="flex h-[100vh] space-x-4 w-full">
+      <Card className="w-[70%]  bg-gray-100">
+        <div className="h-[100vh] ">
           {/* Embed PDF viewer */}
           <iframe
             src={pdfUrl}
             width="100%"
-            height={650}
-            title="CV PDF"
+            height="100%"
             style={{ border: "none", borderRadius: "0.375rem" }}
           ></iframe>
         </div>
       </Card>
 
-      <Card className="px-3 py-5 w-[50%] h-full bg-gray-100 flex flex-col gap-3">
+      <Card className="px-3 py-5  w-[40%] h-full bg-gray-100 flex flex-col gap-3">
         {/* First Part */}
-        <div className="flex-shrink-0">
+        <div className="py-5 sticky top-0 bg-gray-100 z-10">
           <div className="flex justify-between w-[100%] items-start">
-            <div className="flex flex-col w-[60%] flex-wrap pr-3 ">
+            <div className="flex flex-col w-max-[60%] flex-wrap pr-3 ">
               <h1 className="font-bold text-2xl">{data?.name}</h1>
               <p className="font-semibold">{data?.position}</p>
               <p></p>
@@ -84,7 +82,7 @@ const CVDetailPage = ({ params }: { params: string }) => {
               </p>
             </div>
 
-            <div className="flex w-[40%] flex-wrap flex-col gap-2 justify-end">
+            <div className="flex w-max-[40%] flex-wrap flex-col gap-2 justify-end">
               {/* <div className="h-20 bg-white w-32 flex text-center justify-self-end self-end">
                 <Image
                   className="bg-cover"
@@ -95,17 +93,17 @@ const CVDetailPage = ({ params }: { params: string }) => {
                 />
               </div> */}
               <div>
-                <div className="flex gap-1">
-                  <span>Phone Number :</span>
-                  <span>{data?.phone_number}</span>
+                <div className="flex flex-wrap gap-1">
+                  <span>Phone Number:</span>
+                  <span className=" font-semibold">{data?.phone_number}</span>
                 </div>
-                <div className="flex gap-1">
-                  <span>Address :</span>
-                  <span>{data?.address}</span>
+                <div className="flex flex-1 gap-1">
+                  <span>Address:</span>
+                  <span className="font-semibold">{data?.address}</span>
                 </div>
-                <div className="flex gap-1">
-                  <span>Rating :</span>
-                  <span> {data?.rating}</span>
+                <div className="flex flex-1 gap-1">
+                  <span>Rating:</span>
+                  <span className="font-semibold">{data?.rating}</span>
                 </div>
               </div>
             </div>
@@ -114,7 +112,7 @@ const CVDetailPage = ({ params }: { params: string }) => {
         </div>
 
         {/* Second Part (Scrollable) */}
-        <div className="flex-grow overflow-y-auto">
+        <div className="flex-grow overflow-y-auto scrollbar-thin">
           {/* Experience */}
           <div className="flex flex-col gap-2">
             <p className="font-semibold text-xl flex gap-4 ">
