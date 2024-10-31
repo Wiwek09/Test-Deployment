e7cbaecc8efccb5c95f7a3e33d7b001050af023d
Wiwek09 <wastedman01@gmail.com>
2024-10-31 16:09:29 +0545

    refactor: masonry view css and removed sessionStorage

--- components/GridView.tsx
+++ components/GridView.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { ViewContext } from "@/app/dashboard/context/ViewContext";
 import { IFormInputData } from "@/interfaces/FormInputData";
 import { IDocumentData } from "@/interfaces/DocumentData";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 interface GridViewProps {
   data: IDocumentData[];
@@ -13,26 +13,23 @@ interface GridViewProps {
 }
 
 function GridView({ data, searchData }: GridViewProps) {
-  const [imageDataID, setImageDataID] = useState([]);
-  // const [parsedData, setParsedData] = useState<any>([]);
+  const [imageDataID, setImageDataID] = useState<any[]>();
   const contextValue = useContext(ViewContext);
-  const [initialLoad, setInitialLoad] = useState(true);
   const [loading, setLoading] = useState(false);
 
-  const router = useRouter();
+  // useEffect(() => {
+  //   const storedSearchData = sessionStorage.getItem("searchData");
 
-  useEffect(() => {
-    const storedSearchData = sessionStorage.getItem("searchData");
-    if (storedSearchData) {
-      setInitialLoad(false);
-      getFullImageData(JSON.parse(storedSearchData));
-      // getSkillSummary();
-    } else if (data?.length > 0 && !searchData) {
-      setInitialLoad(true);
-    } else {
-      setInitialLoad(false);
-    }
-  }, [data.length, searchData]);
+  //   if (storedSearchData) {
+  //     setInitialLoad(false);
+  //     getFullImageData(JSON.parse(storedSearchData));
+  //     // getSkillSummary();
+  //   } else if (data?.length > 0 && !searchData) {
+  //     setInitialLoad(true);
+  //   } else {
+  //     setInitialLoad(false);
+  //   }
+  // }, [data, searchData]);
 
   // Handle search data and view changes
   useEffect(() => {
@@ -42,6 +39,11 @@ function GridView({ data, searchData }: GridViewProps) {
       setImageDataID([]);
     }
   }, [searchData, contextValue?.view]);
+
+  // useEffect(() => {
+  //   if (imageDataID?.length > 0) {
+  //   }
+  // }, [imageDataID]);
 
   const getFullImageData = async (searchData: IFormInputData) => {
     try {
@@ -87,63 +89,67 @@ function GridView({ data, searchData }: GridViewProps) {
 
   return (
     <div className="masonry-container bg-gray-100">
-      {initialLoad && data?.length > 0 ? (
+      {!searchData && data?.length > 0 ? (
         data?.map((item: any, index) => (
           <div
             key={item.doc_id}
-            onClick={() => router.push(`/cv-detail/${item.doc_id}`)}
             className="masonry-item relative group cursor-pointer"
           >
-            <Image
-              src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/cv_images/${item.image_id}.webp`}
-              alt={`Image ${index + 1}`}
-              height={200}
-              width={300}
-              className="rounded-lg object-cover shadow-lg w-full h-auto"
-              loading="lazy"
-              layout="responsive"
-            />
+            <Link href={`/cv-detail/${item.doc_id}`} target="_blank">
+              <Image
+                src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/cv_images/${item.image_id}.webp`}
+                alt={`Image ${index + 1}`}
+                height={200}
+                width={300}
+                className="rounded-lg object-cover shadow-lg w-full h-auto"
+                loading="lazy"
+                layout="responsive"
+              />
 
-            {/* Overlay that appears on hover */}
-            <div className="absolute inset-0 bg-black bg-opacity-60 rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 backdrop-blur-sm flex items-center justify-center">
-              <div className="text-white text-center p-4">
-                <h3 className="text-lg font-semibold mb-2">Overview</h3>
-                {loading ? (
-                  <div className="flex justify-center items-center">
-                    <div className="spinner-tailwind" role="status">
-                      <span className="sr-only">Loading...</span>
+              {/* Overlay that appears on hover */}
+              <div className="absolute inset-0 bg-black bg-opacity-60 rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 backdrop-blur-sm flex items-center justify-center">
+                <div className="text-white text-center p-4">
+                  <h3 className="text-lg font-semibold mb-2">Overview</h3>
+                  {loading ? (
+                    <div className="flex justify-center items-center">
+                      <div className="spinner-tailwind" role="status">
+                        <span className="sr-only">Loading...</span>
+                      </div>
                     </div>
-                  </div>
-                ) : (
-                  <p className="text-sm">{item.doc_name}</p>
-                )}
+                  ) : (
+                    <p className="text-sm">{item.doc_name}</p>
+                  )}
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
         ))
-      ) : searchData && imageDataID.length > 0 ? (
+      ) : imageDataID?.length > 0 ? (
         imageDataID.map((item: any, index) => (
           <div
             key={index}
-            onClick={() => router.push(`/cv-detail/${item.doc_id}`)}
+            // onClick={() => router.push(`/cv-detail/${item.doc_id}`)}
             className="masonry-item relative group cursor-pointer"
           >
-            <Image
-              src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/cv_images/${item.img_id}.webp`}
-              alt={`Image ${index + 1}`}
-              height={200}
-              width={300}
-              className="rounded-lg object-cover shadow-lg w-full h-auto"
-              loading="lazy"
-              layout="responsive"
-            />
-            {/* Overlay that appears on hover */}
-            <div className="absolute inset-0 bg-black bg-opacity-60 rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 backdrop-blur-sm flex justify-center">
-              <div className="text-white text-center p-4">
-                <h3 className="text-lg font-bold mb-2">Overview</h3>
-                <p className="text-sm">{item.skill_summary}</p>
+            <Link href={`/cv-detail/${item.doc_id}`} target="_blank">
+              <Image
+                src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/cv_images/${item.img_id}.webp`}
+                alt={`Image ${index + 1}`}
+                height={200}
+                width={300}
+                className="rounded-lg object-cover shadow-lg w-full h-auto"
+                loading="lazy"
+                layout="responsive"
+              />
+
+              {/* Overlay that appears on hover */}
+              <div className="absolute inset-0 bg-black bg-opacity-60 rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 backdrop-blur-sm flex justify-center">
+                <div className="text-white text-center p-4">
+                  <h3 className="text-lg font-bold mb-2">Overview</h3>
+                  <p className="text-sm">{item.skill_summary}</p>
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
         ))
       ) : (
